f8ff02324d9022ce7c8b0fb953e7b8165daddff2
Yun <81588081+YunFeng-Zheng@users.noreply.github.com>
2023-03-22 22:00:28 +0100

    Add files via upload

---
+++ DirectMessageApp/src/main/java/com/QuackAttack/DirectMessageApp/DirectMessageAppApplication.java
@@ -0,0 +1,13 @@
+package com.QuackAttack.DirectMessageApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectMessageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectMessageAppApplication.class, args);
+	}
+
+}
