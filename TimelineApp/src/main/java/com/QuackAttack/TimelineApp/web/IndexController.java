decf815b1bd4f197027dceb278bae3c635b88671
Yun Feng Zheng <y.f.zheng@student.utwente.nl>
2023-04-20 16:34:21 +0200

    Added logger

--- TimelineApp/src/main/java/com/QuackAttack/TimelineApp/web/IndexController.java
+++ TimelineApp/src/main/java/com/QuackAttack/TimelineApp/web/IndexController.java
@@ -6,6 +6,8 @@ import com.QuackAttack.TimelineApp.objects.Quack;
 import com.QuackAttack.TimelineApp.objects.RequestFollowingForm;
 import com.QuackAttack.TimelineApp.objects.RequestTimelineForm;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -13,6 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
@@ -25,9 +28,10 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class IndexController {
+    Logger logger = LoggerFactory.getLogger(IndexController.class);
     private static final String GET_FOLLOWING = "http://localhost:8084/getFollowing";
     private static final String GET_QUACKS = "http://localhost:8081/getQuacksByUsername/";
-    private static final int TIMELINE_MAX_SIZE = 10;
+    private static final int TIMELINE_MAX_SIZE = 50;
 
     @Autowired
     RestTemplate restTemplate;
@@ -36,21 +40,22 @@ public class IndexController {
     private TokenVerifier verifier;
 
     @GetMapping("/timeline")
-    public List<Quack> timeline(@CookieValue String credentials) {
+    public ResponseEntity<List<Quack>> timeline(@CookieValue String credentials) {
 
         String username = null;
         try {
             username = verifier.checkToken(credentials);
         } catch (GeneralSecurityException | IOException e) {
-            //return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().build();
         }
-
+        logger.info("Getting timeline for " + username);
         // request to follow service, handle response which is a list of following objects
         // RequestFollowingForm followRequest = new RequestFollowingForm(form.getUser_id());
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Cookie", "credentials="+ credentials);
 
+        logger.info("Getting following for " + username);
         ResponseEntity<List<Following>> response = restTemplate.exchange(
                 GET_FOLLOWING,
                 HttpMethod.GET,
@@ -63,27 +68,33 @@ public class IndexController {
         // request to profile for quacks for each in the list of followings, add them to this list
         List<Quack> quacks = new ArrayList<>();
 
-
+        logger.info("Getting quacks for following of " + username);
         for (Following following : followings) {
+            try {
+                // GET to post a message service to retrieve the quacks per following
+                ResponseEntity<List<Quack>> responseQuacks = restTemplate.exchange(
+                        GET_QUACKS + following.getUser_id(),
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<List<Quack>>() {}
+                );
 
-            // GET to post a message service to retrieve the quacks per following
-            ResponseEntity<List<Quack>> responseQuacks = restTemplate.exchange(
-                    GET_QUACKS + following.getUser_id(),
-                    HttpMethod.GET,
-                    null,
-                    new ParameterizedTypeReference<List<Quack>>() {}
-            );
 
-            List<Quack> followingQuack = responseQuacks.getBody();
-            quacks.addAll(followingQuack);
+                List<Quack> followingQuack = responseQuacks.getBody();
+                quacks.addAll(followingQuack);
+            } catch (RestClientException e) {
+                logger.error("Error in getting following for userID" + following.getUser_id());
+            }
+
+
         }
 
 
         // construct timeline, most recent sort (createdAt)
         Collections.sort(quacks, Comparator.comparing(Quack::getCreated_at).reversed());
         List<Quack> timeline = quacks.subList(0, Math.min(quacks.size(), TIMELINE_MAX_SIZE));
-
-        return timeline;
+        logger.info("Returning for timeline of " + username);
+        return ResponseEntity.ok().body(timeline);
     }
 
 
